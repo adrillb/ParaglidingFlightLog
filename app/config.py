f3628f7baac4234633b1239b295f700e5c8938c7
adrillb <adrian.llana@sdggroup.com>
2025-01-04 10:08:11 +0100

    Ajusts dependencies versions & manages env variables access

--- app/config.py
+++ app/config.py
@@ -10,8 +10,8 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql://username:password@localhost/paragliding'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     
     # Configuraciones de seguridad recomendadas
     SESSION_COOKIE_SECURE = True
